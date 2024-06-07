9ff083aa1dfe11f727e14cdeb5863f5b5ac1cda5
MrPrototyper <157406832+MrPrototyper@users.noreply.github.com>
2024-06-07 10:29:56 +0200

    videos and ssl

--- js/mailer.js
+++ js/mailer.js
@@ -1,56 +1,42 @@
 $(document).ready(function () {
-    // Function to handle login
     function login(callback) {
         $.ajax({
             type: 'POST',
-            url: 'http://54.159.113.234:5000/login', // Replace with your Flask login endpoint URL
+            url: 'https://resourcesasaservice.com/login',
             contentType: 'application/json',
-            data: JSON.stringify({ 'username': 'meuUserSecreto', 'password': 'meuPasswordSecreto' }), // Replace with your actual credentials
-            success: function (response) {
-                // Callback with token
-                console.log(response)
+            data: JSON.stringify({ 'username': 'meuUserSecreto', 'password': 'meuPasswordSecreto' }),            
+            success: function (response) {                
                 callback(response.access_token);
             },
-            error: function (xhr, status, error) {
-                // Handle error response
+            error: function (xhr, status, error) {                
                 alert('Login failed: ' + error);
             }
         });
-    }
-
-    // Function to handle form submission after login
-    $("form[name='contact']").submit(function (event) {
-        // Prevent default form submission
+    }    
+    $("form[name='contact']").submit(function (event) {        
         event.preventDefault();
-
-        // Call login function
-        login(function (token) {
-            // Get form data
+        login(function (token) {            
             var formData = {
-                'recipient': 'tiagoc.mail@gmail.com', // Replace with recipient email
+                'recipient': 'tiagoc.mail@gmail.com',
                 'subject': 'Personal Portfolio Contact Form Submission',
                 'body': $('textarea[name=message]').val(),
                 'sender': {
                     'name': $('input[name=name]').val(),
                     'email': $('input[name=email]').val()
                 }
-            };
-
-            // Send AJAX request to Flask endpoint with JWT token
+            };            
             $.ajax({
                 type: 'POST',
-                url: 'http://54.159.113.234:5000/send_email',
+                url: 'https://resourcesasaservice.com/send_email',
                 headers: {
-                    'Authorization': 'Bearer ' + token // Include JWT token in Authorization header
+                    'Authorization': 'Bearer ' + token
                 },
                 contentType: 'application/json',
                 data: JSON.stringify(formData),
-                success: function (response) {
-                    // Handle successful response
+                success: function (response) {                    
                     alert('Thanks for the contact!');                            
                 },
                 error: function (xhr, status, error) {
-                    // Handle error response
                     alert('Your email could not be sent, please reach out to me on LinkedIn!');
                 }
             });
